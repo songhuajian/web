24f0285dfdfdd07bd674363a611e8b7946ddea2c
宋华健 <2240255905@qq.com>
2019-06-08 13:26:18 +0800

    *学习搭建maven

---
+++ src/main/java/com/springmvc/entity/User.java
@@ -0,0 +1,63 @@
+package com.springmvc.entity;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String userpassword;
+
+    private String usercode;
+
+    private String usercard;
+
+    private String phonenum;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+
+    public String getUsercode() {
+        return usercode;
+    }
+
+    public void setUsercode(String usercode) {
+        this.usercode = usercode;
+    }
+
+    public String getUsercard() {
+        return usercard;
+    }
+
+    public void setUsercard(String usercard) {
+        this.usercard = usercard;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+}
